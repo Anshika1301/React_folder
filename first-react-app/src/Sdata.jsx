ebf842384d120b0095bedf678d5dfa1e34251571
Anshika1301 <tejasianshika@gmail.com>
2020-12-27 14:52:31 +0700

    initial commit

--- first-react-app/src/Sdata.jsx
+++ first-react-app/src/Sdata.jsx
@@ -1,5 +1,6 @@
 const Sdata = [
     {
+        id: '1',
         imgsrc: "https://picsum.photos/300/200",
         title: "Netflix Original Series",
         seriesname: "Dark",
@@ -7,6 +8,7 @@ const Sdata = [
     },
 
     {
+        id: '2',
         imgsrc: "https://picsum.photos/seed/picsum/300/200",
         title: "Netflix Original Series",
         seriesname: "Extra Curricular",
@@ -14,9 +16,17 @@ const Sdata = [
     },
 
     {
+        id: '3',
         imgsrc: "https://picsum.photos/300/200/?blur",
         title: "Netflix Original Series",
         seriesname: "Stranger Things",
+        link: "https://picsum.photos/",
+    },
+    {
+        id: '4',
+        imgsrc: "https://picsum.photos/seed/picsum/300/200",
+        title: "Netflix Original Series",
+        seriesname: "Extra Curricular",
         link: "https://picsum.photos/",
     },
 ];
